df2dceccd1eb6447540de878bba7767ba2ea2505
Paulo Ferreira <phgferreira@outlook.com>
2023-04-23 22:39:56 -0300

    Corrigido problema de estilo com tailwindcss

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,21 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a Home</h1>
-<!--  <custom-header-->
-<!--    @create-account="handleAccountCreate"-->
-<!--    @login="handleLogin"/>-->
-<!--  <contact/>-->
-<!--  <footer class="flex justify-center py-10 bg-brand-gray">-->
-<!--    <p class="font-medium text-center text-gray-800">feedbacker 2023</p>-->
-<!--  </footer>-->
+  <custom-header
+    @create-account="handleAccountCreate"
+    @login="handleLogin"/>
+  <contact/>
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker 2023</p>
+  </footer>
 </template>
 
 <script>
-// import CustomHeader from '@/views/home/CustomHeader.vue'
-// import Contact from '@/views/home/Contact.vue'
+import CustomHeader from '@/views/home/CustomHeader.vue'
+import Contact from '@/views/home/Contact.vue'
 import { useRouter } from 'vue-router'
 import { onMounted } from 'vue'
 
 export default {
-  components: { },
+  components: { CustomHeader, Contact },
   setup () {
     const router = useRouter()
     onMounted(() => {
