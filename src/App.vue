cab7a16ce5e7dced40cef8c77b8cb62225d9c2f8
Paulo Henrique Gonçalves Ferreira <phgferreira@outlook.com>
2023-05-18 21:04:11 -0300

    Pegando informações do usuário logado

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,31 @@
 
 <script>
 import ModalFactory from '@/components/ModalFactory/index.vue'
+import { useRoute, useRouter } from 'vue-router'
+import { watch } from 'vue'
+import services from './services'
 
 export default {
-  components: { ModalFactory }
+  components: { ModalFactory },
+  setup () {
+    // Realiza ações relacionadas a rotas
+    const router = useRouter()
+    // Pega as informações da rota atual
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+
+        const data = await services.users.getMe()
+        console.log(data)
+      }
+    })
+  }
 }
 </script>
