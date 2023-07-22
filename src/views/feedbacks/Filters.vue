97fd976b49b36f54171d4eff226b746783c737da
Paulo Ferreira <phgferreira@outlook.com>
2023-07-22 18:54:01 -0300

    Corrigido uso de classes para funcionamento do correto do purge do tailwind

--- src/views/feedbacks/Filters.vue
+++ src/views/feedbacks/Filters.vue
@@ -11,13 +11,13 @@
       >
         <div class="flex items-center">
         <span
-          :class="`bg-${filter.color}`"
+          :class="filter.color.bg"
           class="inline-block w-2 h-2 rounded-full mr-2"
         />
           {{ filter.label }}
         </div>
         <span
-          :class="filter.active ? `text-${filter.color}` : 'text-brand-graydark'"
+          :class="filter.active ? filter.color.text : 'text-brand-graydark'"
           class="font-bold"
         >
           {{ filter.amount }}
@@ -40,10 +40,10 @@ const LABELS = {
 }
 
 const COLORS = {
-  all: 'brand-info',
-  issue: 'brand-danger',
-  idea: 'brand-warning',
-  other: 'brand-graydark'
+  all: { text: 'text-brand-info', bg: 'bg-brand-info' },
+  issue: { text: 'text-brand-danger', bg: 'bg-brand-danger' },
+  idea: { text: 'text-brand-warning', bg: 'bg-brand-warning' },
+  other: { text: 'text-brand-graydark', bg: 'bg-brand-graydark' }
 }
 
 export default {
